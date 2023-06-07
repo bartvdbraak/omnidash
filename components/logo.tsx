91cd7f05d0c25025b0e508786b85c1c60bba6bc7
Bart van der Braak <bartvdbraak@gmail.com>
2023-06-07 08:49:57 +0200

    feat: Changed logo and icon

--- components/logo.tsx
+++ components/logo.tsx
@@ -5,16 +5,21 @@ export const Logo: React.FC<Props> = ({ className }) => {
   return (
     <svg
       className={className}
-      viewBox="0 0 512 384"
-      stroke="current"
       xmlns="http://www.w3.org/2000/svg"
-    >
-      {/* <rect width="512" height="384" /> */}
-      <path d="M96 64L416 64" strokeWidth="32" strokeLinecap="round" />
-      <path d="M147 128L371 128" strokeWidth="32" strokeLinecap="round" />
-      <path d="M221 192L381 192" strokeWidth="32" strokeLinecap="round" />
-      <path d="M242 256L338 256" strokeWidth="32" strokeLinecap="round" />
-      <path d="M227 320L259 320" strokeWidth="32" strokeLinecap="round" />
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="current"
+      stroke-width="2"
+      stroke-linecap="round"
+      stroke-linejoin="round">
+      <rect width="7" height="9" x="3" y="3" rx="1"/>
+      <rect width="7" height="5" x="14" y="3" rx="1"/>
+      <rect width="7" height="9" x="14" y="12" rx="1"/>
+      <rect width="7" height="5" x="3" y="16" rx="1"/>
     </svg>
   );
 };
+
+
