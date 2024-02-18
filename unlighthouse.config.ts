ab161e0b91af1b38c1916018847b86cc4f3a0177
Bart van der Braak <bartvdbraak@gmail.com>
2024-02-18 22:21:07 +0100

    feat: unlighthouse configuration and dependencies

---
+++ unlighthouse.config.ts
@@ -0,0 +1,23 @@
+import { defineConfig } from 'unlighthouse'
+
+export default defineConfig ({
+  site: process.env.SITE_URL || 'http://localhost:5173/',
+  scanner: {
+    samples: 1,
+    throttle: true,
+  },
+  cookies: [
+    {
+      name: 'pb_auth',
+      value: process.env.AUTH_COOKIE || '',
+      domain: 'localhost',
+      path: '/',
+      sameSite: 'Lax',
+    }
+  ],
+  ci: {
+    reporter: 'jsonExpanded',
+    buildStatic: true,
+    budget: 65,
+  },
+});
