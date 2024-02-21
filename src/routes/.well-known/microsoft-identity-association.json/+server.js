a671ef73ab8b586155585a429d9ba7cee421cce1
Bart van der Braak <bartvdbraak@gmail.com>
2024-02-22 00:47:58 +0100

    fix: parsing issue in server.js

--- src/routes/.well-known/microsoft-identity-association.json/+server.js
+++ src/routes/.well-known/microsoft-identity-association.json/+server.js
@@ -1,7 +1,7 @@
 import { MSID_ASSOCIATION } from '$env/static/private';
 
 export async function GET() {
-	return new Response(JSON.stringify(MSID_ASSOCIATION), {
+	return new Response(JSON.stringify(JSON.parse(MSID_ASSOCIATION)), {
 		headers: {
 			'Cache-Control': `public, max-age=${86400}`,
 			'Content-Type': 'application/json'
